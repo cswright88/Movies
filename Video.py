09de75779ef819f3cd94382e0ee67f9439216e93
Chris Wright <cswright88@gmail.com>
2017-05-07 20:54:39 -0500

    add other python files

---
+++ Video.py
@@ -0,0 +1,8 @@
+class Video():
+    #creat title
+    def __init__(self, movie_title, movie_duration):
+        print ("called parent video")
+        self.movie_title = movie_title
+        self.movie_duration = movie_duration
+    #create durration of film/tv show
+    
---
+++ movieMain.py
@@ -0,0 +1,21 @@
+import movie
+import fresh_tomatoes
+
+mr_nobody=movie.Movie("Mr. Nobody", "One Life, many choices, many paths", "https://upload.wikimedia.org/wikipedia/en/3/32/Mr._Nobody_%28film_poster%29.jpg", "https://www.youtube.com/watch?v=mpi0qsp3v_w")
+
+avatar=movie.Movie("Avatar", "Mariene goes to an alien planet to learn about the locals", "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+revolver=movie.Movie("Revolver", "A man goes on a journy to let go of his ego", "https://upload.wikimedia.org/wikipedia/en/b/b6/Revolver2005.jpg", "https://www.youtube.com/watch?v=M9JAYfZOHms")
+
+fight_club=movie.Movie("Fight Club", "Do not talk about Fight Club", "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg", "https://www.youtube.com/watch?v=SUXWAEX2jlg")
+
+the_karate_kid=movie.Movie("The Karate Kid", "A boy learns to no longer be afraid", "https://upload.wikimedia.org/wikipedia/en/d/d5/Karate_kid_ver2.jpg", "https://www.youtube.com/watch?v=2SmmxvHLsKk")
+
+peaceful_warrior=movie.Movie("Peaceful Warrior", "Story of an olympic hopful and his journey to be the lord of the rings", "https://upload.wikimedia.org/wikipedia/en/9/9c/Peaceful_warrior.jpg", "https://www.youtube.com/watch?v=gegNMYvY_yg")
+
+movies = [mr_nobody, avatar, revolver, fight_club, the_karate_kid, peaceful_warrior]
+
+#fresh_tomatoes.open_movies_page(movies)
+#print (movie.Movie.VALID_RATINGS)
+#print (movie.Movie.__doc__)
+#print (movie.Movie.__module__)
